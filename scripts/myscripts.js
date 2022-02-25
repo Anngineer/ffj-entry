4ba9e1cdb307febffbd577843f66dd7ad3337dc9
Ann <anngineer@gmail.com>
2022-02-24 21:46:49 -0500

    Adds footer as well as JS to look for the height of the section after the message is added, then to set the encompassing section larger than that

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -2,11 +2,18 @@
 // When the page loads, check to see if it has a hash in the website
 let myunDecodedURLHash = window.location.hash.substring(1);
 let myDecodedURLHash = atob(myunDecodedURLHash);
-console.log(`the secret message is ${myDecodedURLHash}`);
+// console.log(`the secret message is ${myDecodedURLHash}`);
 let message = document.querySelector("#message");
-message.innerText = myDecodedURLHash;
-// I'll be setting a <p> </p> to have the decoded text.
-// Have a hyperlink to take you to the journal page to write another one
 
-// Maybe after the page loads, find out what the height of the body is
-//and then add an extra 10rem to it;
+let messageDecoderSection = document.querySelector("#message-decoder-section");
+let height = message.offsetHeight;
+message.innerText = myDecodedURLHash;
+
+setTimeout(() => {
+  height = message.offsetHeight;
+  //   console.log(`the height of the message is ${height}`);
+  let newHeight = message.offsetHeight + 1000;
+  messageDecoderSection.style.height = `${newHeight}px`;
+  //   console.log(`The section height is ${messageDecoderSection.clientHeight}`);
+  //   console.log(newHeight);
+}, 100);
